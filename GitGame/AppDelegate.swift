baefa55b9b315a9ae80ebe48dcf7a6963be6e81a
ajackson66 <ajackson66@wolf.cheyney.edu>
2017-05-18 14:25:52 -0400

    Initial Commit

---
+++ GitGame/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GitGame
+//
+//  Created by Anitra Jackson on 5/18/17.
+//  Copyright © 2017 Anitra Jackson. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
